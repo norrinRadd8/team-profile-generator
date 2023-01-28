90e9e0d699ed254f5f883b440328eeb8daffa701
H <h@Hs-MacBook-Air.local>
2023-01-28 16:50:10 +0000

    installed inquirer@^8.0.0

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+// const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+// const alice = new Manager('Alice', '1', 'test@test.com')
+
+// alice.getName()
+// alice.getId()
+// alice.getEmail()
+
+inquirer.prompt([
+    {
+    name: "name",
+    message: "Please enter your name.",
+    validate: (user_input) => {
+        if(!user_input.length) return 'You must enter at least one character.'
+        return true
+    }
+
+    }
+]) .then(data => {
+    console.log(data)
+})
+
 
