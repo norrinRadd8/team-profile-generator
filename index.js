700aeb71508d7d3400bb55cdb61436a815c64425
H <h@Hs-MacBook-Air.local>
2023-01-31 18:28:30 +0000

    updated code

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-const { type } = require("os");
 
 const teamData = []
 
